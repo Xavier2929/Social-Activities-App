c1d7378dc21023db6525d12f0080780831d52d0c
Xavier2929 <castilloxavier49@gmail.com>
2025-08-28 23:45:36 -0700

    UPDATE: added a React project under the name client, and changed its default port.

--- Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b7d5a46e1c61f033a630585d4e21f232e95433d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5182625113f89842991250a5dbb5df00f678e33")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
