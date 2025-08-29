3f16ddf22c597a3bc6e9cf35aa9b9e4abb1aec61
Xavier2929 <castilloxavier49@gmail.com>
2025-08-29 00:49:51 -0700

    UPDATE: moved the client project (React App) it didn't appear

--- Persistence/obj/Debug/net9.0/Persistence.AssemblyInfo.cs
+++ Persistence/obj/Debug/net9.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1d7378dc21023db6525d12f0080780831d52d0c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2469279db8f9f90828b6c377ece8db3ce15a36d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
