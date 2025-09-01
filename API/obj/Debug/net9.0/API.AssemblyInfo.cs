07abc4fa48b532e85a6a6450dd23d8b5c008c71d
Xavier2929 <castilloxavier49@gmail.com>
2025-08-31 18:40:50 -0700

    UPATE: added the images folders on the public react folder. And now we have a demo on how the detail card will show once we click on the view button of a activity.

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faf6b883e6c39aa1bd2ad1427e6e3d4fced8eb1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42fa37e3c85ef4b40c6bb8751bd15e95d03bdbf4")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
